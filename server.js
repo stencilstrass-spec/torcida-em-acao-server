45db2cf95b48961eeb3bdb11a0b01d129d523365
stencilstrass-spec <stencilstrass@gmail.com>
2026-03-10 15:33:38 -0300

    Update server.js

--- server.js
+++ server.js
@@ -97,6 +97,7 @@ io.on("connection", (socket) => {
           `🎁 Gift: "${giftName}" → ${
             mapping ? `${mapping.teamId} (+${mapping.points}pts)` : "NÃO MAPEADO"
           } de ${giftData.username}`
+          console.log(`🖼️ Imagem: ${giftData.giftPictureUrl}`);
         );
 
         io.emit("gift", giftData);
